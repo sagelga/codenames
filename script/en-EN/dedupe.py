026972193bc2f5020d96ed4a3aac7aa73d12b69e
Kunanon Srisuntiroj <son9912@gmail.com>
2021-03-29 11:50:29 +0700

    Update dedupe.py

--- script/en-EN/dedupe.py
+++ script/en-EN/dedupe.py
@@ -1,16 +1,49 @@
 import getopt
-import argparse
 import sys
+import argparse
+
+
+def main():
+    # Options
+    options = "i:o:"
+    long_options = ["input", "output"]
+
+    input_dir = "en-EN"
+    output_dir = "new.log"
+
+    try:
+        # Parsing argument
+        arguments, values = getopt.getopt(sys.argv[1:], options, long_options)
+
+        # checking each argument
+        for currentArgument, currentValue in arguments:
+            if currentArgument in ("-i", "--input"):
+                input_dir = currentValue
+            elif currentArgument in ("-o", "--output"):
+                output_dir = currentValue
+
+    except getopt.error as err:
+        # output error, and return with an error code
+        print(str(err))
+
+    # Initialize parser
+    parser = argparse.ArgumentParser(description="Adding description")
+    parser.add_argument("-i", "--input", help="Show Output")
+    parser.add_argument("-o", "--output", help="Show Output")
+    parser.parse_args()
+
+    start(prefix=input_dir, new_file=output_dir, file_name=input())
 
 
 def start(prefix="en-EN/", file_name=input(), suffix="/wordlist.txt", new_file="/new.txt"):
     prefix = "wordlist/" + prefix
 
-    result = dedupe("{}{}{}".format(prefix, file_name, suffix))
-    print(result["wordlist"])
+    result = dedupe("{}/{}/{}".format(prefix, file_name, suffix))
 
     if len(result["dupe"]):
-        f = open("{}{}{}dupe.txt".format(prefix, file_name, suffix), "a")
+        print("Duplicates Detected: {}".format(result["dupe"]))
+
+        f = open("{}/{}/{}".format(prefix, file_name, duplicate_file), "w")
         f.write("Duplicates detected\n")
         f.write("\n".join([str(x) for x in result["dupe"]]))
 
@@ -19,51 +52,22 @@ def start(prefix="en-EN/", file_name=input(), suffix="/wordlist.txt", new_file="
 
 
 def dedupe(file_name):
-    print(file_name)
-    f = open(file_name, 'r')
+    f = open(file_name, 'r').read().splitlines()
 
     wordlist = list()
     dupe = list()
 
-    for x in f:
-        x = x.capitalize().rstrip("\n")
-        if x not in wordlist:
-            wordlist.append(x)
+    for x in range(len(f)):
+        y = f[x].lstrip(" ").rstrip(" ").lower()
+        if wordlist.count(y):
+            dupe.append(y.capitalize())
         else:
-            dupe.append(x)
+            wordlist.append(y.capitalize())
 
     wordlist.sort()
 
     return {"wordlist": wordlist, "dupe": dupe}
 
 
-# Options
-options = "i:o:"
-long_options = ["input", "output"]
-
-input_dir = str()
-output_dir = str()
-
-try:
-    # Parsing argument
-    arguments, values = getopt.getopt(sys.argv[1:], options, long_options)
-
-    # checking each argument
-    for currentArgument, currentValue in arguments:
-        if currentArgument in ("-i", "--input"):
-            input_dir = currentValue
-        elif currentArgument in ("-o", "--output"):
-            output_dir = currentValue
-
-except getopt.error as err:
-    # output error, and return with an error code
-    print(str(err))
-
-
-# Initialize parser
-parser = argparse.ArgumentParser(description="Adding description")
-parser.add_argument("-i", "--input", help="Show Output")
-parser.add_argument("-o", "--output", help="Show Output")
-parser.parse_args()
-
-start(prefix=input_dir, suffix=new_file)
+if __name__ == "__main__":
+    main()
